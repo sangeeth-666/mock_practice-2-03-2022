2f2f6707e4c5ed03632e0f0936f56a4398556047
sangeeth <sangeeth@gmail.com>
2022-03-02 21:49:48 +0530

    "To check the condition even or odd"

---
+++ enum.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int funcheck(int num);
+
+enum check{no,yes};//enum default to set 0,1 so no=1,yes=1
+
+int main()
+{
+    int num,result;
+    printf("enter the number:");
+    scanf("%d",&num);
+    result=funcheck(num);
+    if(result==no)
+    printf("The given number is odd\n");
+    else
+    printf("The given number is even\n");
+}
+int funcheck(int num)
+{
+    if(num%2==no)
+    return yes;
+    else
+    return no;
+}
+
